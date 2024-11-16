e1979f0e6e151b939d9f1187798a071124aaf11c
Luis-Lopez-Pineda <lf.pineda@unah.hn>
2024-11-16 11:07:28 -0600

    Modelo de Usuarios implementado.

--- lib/api/users/users_registered.dart
+++ lib/api/users/users_registered.dart
@@ -1,18 +1,20 @@
-final List<Map<String, dynamic>> usersRegistered = [
-  {
-    "id": "2b5bafa9-9e65-4c6b-8f64-febe5d2abdbf",
-    "email": "cristian.delcid@unah.hn",
-    "password": "20142002062",
-    "phone": "99999999",
-    "name": "Cristian Delcid",
-    "profile": "assets/cristian_delcid.jpg"
-  },
-  {
-    "id": "e32e5d1a-ee73-46ba-981f-f8826164ee95",
-    "email": "lf.pineda@unah.hn",
-    "password": "20212020009",
-    "phone": "33333333",
-    "name": "Luis Pineda",
-    "profile": "assets/luis_pineda.jpg"
-  },
+import 'package:uuid/uuid.dart';
+
+import 'package:login_app/api/models/user.model.dart';
+
+final List<User> usersRegistered = [
+  User(
+      id: const Uuid().v4(),
+      email: "cristian.delcid@unah.hn",
+      password: "20142002062",
+      phone: "99999999",
+      name: "Cristian Delcid",
+      profile: "assets/cristian_delcid.jpg"),
+  User(
+      id: const Uuid().v4(),
+      email: "lf.pineda@unah.hn",
+      password: "20212020009",
+      phone: "33333333",
+      name: "Luis Pineda",
+      profile: "assets/luis_pineda.jpg"),
 ];
