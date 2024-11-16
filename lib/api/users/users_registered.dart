901039c5fad61e8b1cc746f0a850275a0fd779fe
“Cristian <cristian_delcid10@yahoo.com>
2024-11-16 01:57:43 -0600

    feat: informacion de usuarios iniciales registrados

---
+++ lib/api/users/users_registered.dart
@@ -0,0 +1,18 @@
+final List<Map<String, dynamic>> usersRegistered = [
+  {
+    "id": "2b5bafa9-9e65-4c6b-8f64-febe5d2abdbf",
+    "email": "cristian.delcid@unah.hn",
+    "password": "20142002062",
+    "phone": "99999999",
+    "name": "Cristian Delcid",
+    "profile": "assets/cristian_delcid.jpg"
+  },
+  {
+    "id": "e32e5d1a-ee73-46ba-981f-f8826164ee95",
+    "email": "lf.pineda@unah.hn",
+    "password": "20212020009",
+    "phone": "33333333",
+    "name": "Luis Pineda",
+    "profile": "assets/luis_pineda.jpg"
+  },
+];
